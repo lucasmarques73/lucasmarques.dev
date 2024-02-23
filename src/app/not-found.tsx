086e6c6f3bd170558d2c2619b97cf8b07f2ff82d
Lucas Marques <lucasmarques73@hotmail.com>
2024-02-22 23:41:14 -0300

    create not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page";
+import { getContentBySlug } from "@/repository";
+
+
+export default async function NotFound() {
+    const data = await getContentBySlug('pages', '404')
+
+    return (
+        <Page content={data?.content} />
+    );
+}
+
