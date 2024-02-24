04bc7c104bcdd6f885ea2a23f6aff7f78bb76de6
Lucas Marques <lucasmarques73@hotmail.com>
2024-02-24 20:59:45 -0300

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         <main className="mx-auto  max-w-2xl px-6 ">
           {children}
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
