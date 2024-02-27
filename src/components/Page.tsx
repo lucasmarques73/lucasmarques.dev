0705f2e1130f9cdc0d6ae5861efb421b76783c5d
Lucas Marques <lucasmarques73@hotmail.com>
2024-02-27 16:56:19 -0300

    using highlightjs

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,13 @@
-export default async function Page({ content }: { content: string | undefined }) {
+'use client'
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark-reasonable.css';
 
+import { useEffect } from 'react';
+
+export default function Page({ content }: { content: string | undefined }) {
+    useEffect(() => {
+        hljs.initHighlighting();
+    }, [content]);
     return (
         <article className="prose">
             <div dangerouslySetInnerHTML={{ __html: content || '' }} />
