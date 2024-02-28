82a9bc6a7c69e69f2080c93ec535138d87b93bae
Lucas Marques <lucasmarques73@hotmail.com>
2024-02-27 22:04:01 -0300

    add Highlightjs (#1)
    * using highlightjs
    
    * add dracula theme

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,13 @@
-export default async function Page({ content }: { content: string | undefined }) {
+'use client'
+import hljs from 'highlight.js';
+import '../app/dracula.css';
 
+import { useEffect } from 'react';
+
+export default function Page({ content }: { content: string | undefined }) {
+    useEffect(() => {
+        hljs.initHighlighting();
+    }, [content]);
     return (
         <article className="prose">
             <div dangerouslySetInnerHTML={{ __html: content || '' }} />
