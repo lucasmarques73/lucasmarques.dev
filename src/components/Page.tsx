61fd7a1f523d153bf74401c73c85ff5078c67e31
Lucas Marques <lucasmarques73@hotmail.com>
2024-02-28 23:20:44 -0300

    use correct method

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export default function Page({ content }: { content: string | undefined }) {
     useEffect(() => {
-        hljs.initHighlighting();
+        hljs.highlightAll();
     }, [content]);
     return (
         <article className="prose">
