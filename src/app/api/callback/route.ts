c52dcd842529e8636b79f36647802df2cf925300
Lucas Marques <lucasmarques73@hotmail.com>
2024-03-13 12:05:24 -0300

    Turn ISR function to Dynamic (#7)
    * Chante ISR function to dynamic
    
    * remove unecessary runtime

--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -1,6 +1,8 @@
 import { callback } from "@/app/api";
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export function GET(request: NextRequest) {
   return callback(request);
 }
