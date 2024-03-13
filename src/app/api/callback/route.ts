096b9777fe9bb8ede3f1e699ba6fe79601d959ed
Lucas Marques <lucasmarques73@hotmail.com>
2024-03-13 11:55:03 -0300

    Chante ISR function to dynamic

--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -1,6 +1,9 @@
 import { callback } from "@/app/api";
 import { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 export function GET(request: NextRequest) {
   return callback(request);
 }
